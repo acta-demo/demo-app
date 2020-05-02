0715d15cb232faa280c76f4cc56876987769de3d
ELEFTHERIOU Dimitrios <eledim@intrasoft-intl.com>
2020-05-02 10:49:18 +0200

    prettier & js-beautify

--- src/app/multiroot-editor/modal-date-picker/modal-date-picker.component.ts
+++ src/app/multiroot-editor/modal-date-picker/modal-date-picker.component.ts
@@ -12,6 +12,7 @@ export class ModalDatePickerComponent implements OnInit {
     @Input() fromParent: any;
 
     dateSelected: NgbDate | null = null;
+
     faWindowClose = faWindowClose;
 
     constructor(public activeModal: NgbActiveModal, private calendar: NgbCalendar) {}
